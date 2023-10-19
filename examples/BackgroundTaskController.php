df9e3ee1f95f228ce7132d984e0c6a2e94aaa2c3
Petr Sleptsov <spetr@bk.ru>
2023-10-20 00:11:15 +0900

    Пример кода, запускающего тяжелую задачу в "фоне" из вызываемого экшна контроллера

---
+++ examples/BackgroundTaskController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\controllers;
+
+
+// ...
+
+
+/**
+ * Description of ImportController
+ *
+ * @author demiurg
+ */
+class BaskgroundTaskController extends Controller {
+
+    // ...
+
+    public function actionIndex($id) {
+
+        // ...
+
+        // Подписываемся на соответствующее событие для запуска задачи после отправки
+        // всех данных клиенту
+        \Yii::$app->response->on(Response::EVENT_AFTER_SEND, [$this, 'afterSend'], $form);
+
+        // ...
+
+    }
+
+    // ...
+
+    public function afterSend(Event $event) {
+        try {
+            // Закрываем сессию
+            session_write_close();
+            // Делаем бессрочным длительность выполнения текущего скрипта
+            set_time_limit(0);
+            // Игнорируем закрытие соединения клиента
+            ignore_user_abort(true);
+            // Очищаем буфер
+            flush();
+            // Сбрасывает все запрошенные данные клиенту (из документации)
+            fastcgi_finish_request();
+            
+            // Ниже код задачи, которая должна испольняться в фоне
+            // ...
+        }
+        catch(\Exception $e) {
+
+            // ...
+
+        }
+        finally {
+
+            // ...
+
+        }
+    }
+
+    // ...
+
+}
