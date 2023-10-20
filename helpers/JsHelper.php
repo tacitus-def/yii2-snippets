e38b3712d2ec80a71fd7f28a18fe77e86352ce65
Petr Sleptsov <spetr@bk.ru>
2023-10-20 10:19:54 +0900

    Убран лишний символ из проверочного regex-выражения в JsHelper::setVars()

--- helpers/JsHelper.php
+++ helpers/JsHelper.php
@@ -16,7 +16,7 @@ class JsHelper {
     public static function setVars($view, $vars) {
         $js = "if((!window.jsVars)||typeof window.jsVars!=='object'){window.jsVars={};}";
         foreach ($vars as $key => $value) {
-            if (!preg_match('/^[a-z_]+[a-z0-9-_]*$/i', $key)) {
+            if (!preg_match('/^[a-z_]+[a-z0-9_]*$/i', $key)) {
                 continue;
             }
             $js .= "window.jsVars.{$key} = " . (json_encode($value) ?: 'null') . ';';
