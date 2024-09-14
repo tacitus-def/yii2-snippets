0bf85a3a6d6eedc0b64584f85cbeae2a74c77c3a
knyaz <spetr@bk.ru>
2024-09-14 18:28:46 +0300

    Класс Chaining для обработки переменных и значений в цепочке

---
+++ components/Chaining.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\components;
+
+class Chaining {
+        private $value;
+
+        private function __construct(&$value) {
+                $this->value = &$value;
+        }
+
+        public function __call($func, $args) {
+                list($callback, $position) = explode(':', $func) + [null, 0];
+                $data = array_merge(array_splice($args, 0, $position) ?? [], [$this->value], $args ?? []);
+                $this->value = call_user_func_array($callback, $data);
+
+                return $this;
+        }
+
+        public static function ref(&$value) {
+            return new self($value);
+        }
+
+        public static function set($value) {
+            return new self($value);
+        }
+
+        public function get() {
+            return $this->value;
+        }
+}
